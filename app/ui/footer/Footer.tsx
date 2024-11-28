535d5d8f838b7a2efcdbbba63f88e04beac18ab7
Darshan Chobarkar <dchobarkar@gmail.com>
2024-11-28 13:06:20 +0530

    add footer

--- app/ui/footer/Footer.tsx
+++ app/ui/footer/Footer.tsx
@@ -1,5 +1,11 @@
 const Footer = () => {
-  return <div>This is Footer</div>;
+  return (
+    <footer className="bg-primary">
+      <div className="container mx-auto px-6 py-4 text-center">
+        <p>&copy; 2024 DarshanWebDev. All rights reserved.</p>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
