f0217e51205014485d8a4a27e1d115b73a5fb704
Darshan Chobarkar <dchobarkar@gmail.com>
2024-11-29 15:20:16 +0530

    add categories section structure

--- app/lib/constant.ts
+++ app/lib/constant.ts
@@ -1,3 +1,27 @@
 const navLinks = [{ label: "Categories", href: "/blog" }];
 
-export { navLinks };
+const categoryCards = [
+  {
+    title: "Category 1",
+    image: "/category-1.png",
+    description:
+      "Lorem ipsum dolor sit amet, ea vix alii epicurei, an pro mutat alienum similique. Blandit mandamus pro at, usu vide splendide sententiae et, ut nam consul lobortis. Vim ne falli prompta laoreet, vix graeco minimum accommodare in. Omnium detraxit intellegam ius te, euismod invidunt cum ne. Sea ancillae pertinax ut,",
+    href: "/blogs/1",
+  },
+  {
+    title: "Category 2",
+    image: "/category-1.png",
+    description:
+      "Lorem ipsum dolor sit amet, ea vix alii epicurei, an pro mutat alienum similique. Blandit mandamus pro at, usu vide splendide sententiae et, ut nam consul lobortis. Vim ne falli prompta laoreet, vix graeco minimum accommodare in. Omnium detraxit intellegam ius te, euismod invidunt cum ne. Sea ancillae pertinax ut,",
+    href: "/blogs/2",
+  },
+  {
+    title: "Category 3",
+    image: "/category-1.png",
+    description:
+      "Lorem ipsum dolor sit amet, ea vix alii epicurei, an pro mutat alienum similique. Blandit mandamus pro at, usu vide splendide sententiae et, ut nam consul lobortis. Vim ne falli prompta laoreet, vix graeco minimum accommodare in. Omnium detraxit intellegam ius te, euismod invidunt cum ne. Sea ancillae pertinax ut,",
+    href: "/blogs/3",
+  },
+];
+
+export { navLinks, categoryCards };
--- app/ui/components/categories.tsx
+++ app/ui/components/categories.tsx
@@ -1,5 +1,24 @@
+import { categoryCards } from "@/app/lib/constant";
+import CategoryCard from "./CategoryCard";
+
 const Categories = () => {
-  return <section>This is categories section</section>;
+  return (
+    <section className="container m-auto w-full min-h-screen px-6 py-4 flex flex-col justify-center items-center space-y-3">
+      <h1>Browse by Categories</h1>
+
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 w-full">
+        {categoryCards.map((category) => (
+          <CategoryCard
+            key={category.title}
+            title={category.title}
+            des={category.description}
+            img={category.image}
+            href={category.href}
+          />
+        ))}
+      </div>
+    </section>
+  );
 };
 
 export default Categories;
