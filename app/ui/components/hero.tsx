49b5c4b48594c23b45a783a509594d47d166b884
Darshan Chobarkar <dchobarkar@gmail.com>
2024-11-29 13:04:03 +0530

    add hero section structure

--- app/ui/components/hero.tsx
+++ app/ui/components/hero.tsx
@@ -1,5 +1,18 @@
+import Link from "next/link";
+
 const Hero = () => {
-  return <section>Hero section</section>;
+  return (
+    <section className="container m-auto w-full h-screen px-6 py-4 flex flex-col justify-center items-center space-y-3">
+      <h1>Welcome to DarshanWebDev Blog</h1>
+
+      <h3>Sharing insights into Tech and more</h3>
+
+      <div className="w-full flex justify-around">
+        <Link href="/blogs">Explore Blogs</Link>
+        <Link href="/blogs">Learn More</Link>
+      </div>
+    </section>
+  );
 };
 
 export default Hero;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -20,12 +20,6 @@ export default {
         heading: ["var(--font-montserrat)"], // For headings
         sans: ["var(--font-roboto)"],
       },
-      fontSize: {
-        h1: ["36px", { lineHeight: "1.2", fontWeight: "700" }], // Bold
-        h2: ["28px", { lineHeight: "1.3", fontWeight: "600" }], // Semi-Bold
-        h3: ["20px", { lineHeight: "1.4", fontWeight: "600" }], // Semi-Bold
-        body: ["16px", { lineHeight: "1.5", fontWeight: "400" }], // Regular
-      },
     },
   },
   plugins: [],
