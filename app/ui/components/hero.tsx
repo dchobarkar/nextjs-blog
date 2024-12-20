16bbf50103b9ea793ffc63e44bab4d24b20204ee
Darshan Chobarkar <dchobarkar@gmail.com>
2024-12-20 12:49:03 +0530

    fix styling

--- app/ui/components/hero.tsx
+++ app/ui/components/hero.tsx
@@ -3,9 +3,9 @@ import Link from "next/link";
 const Hero = () => {
   return (
     <section className="container m-auto w-full h-screen px-6 py-4 flex flex-col justify-center items-center space-y-3">
-      <h1>Welcome to DarshanWebDev Blog</h1>
+      <h1 className="text-center">Welcome to DarshanWebDev Blog</h1>
 
-      <h3>Sharing insights into Tech and more</h3>
+      <h3 className="pb-20 text-center">Sharing insights into Tech and more</h3>
 
       <div className="w-full flex justify-around">
         <Link href="/blogs">Explore Blogs</Link>
